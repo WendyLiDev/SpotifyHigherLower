12d0d54247eb88453f432caba725c08aa406e6a1
wendylidev <wendyli.contact@gmail.com>
2024-06-02 18:36:25 -0700

    Integrated typescript and set default values for tracks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,12 @@ import './App.css';
 import axios from 'axios';
 import React, { useState } from 'react';
 
-function StartButton({onStart}) {
+type Track = {
+  name: string;
+  id: number;
+};
+
+function StartButton({onStart}:any) {
   return (
     <button id="startGame" onClick={onStart}>
       Start
@@ -10,14 +15,22 @@ function StartButton({onStart}) {
   );
 };
 
-function Game({onEnd, firstSong, secondSong}) {
+interface GameProps {
+  onEnd: () => void,
+  firstSong: Track,
+  secondSong: Track
+};
+
+const Game:React.FC<GameProps> = ({onEnd, firstSong, secondSong}) => {
   return (
-    <div id="game" display="none" visibility="hidden">
+    <div id="game">
       <div>
-        <h3>{firstSong}</h3>
+        <h3>{firstSong.name}</h3>
+        <h4>{firstSong.id}</h4>
       </div>
       <div>
-        <h3>{secondSong}</h3>
+        <h3>{secondSong.name}</h3>
+        <h4>{secondSong.id}</h4>
         <button id="higherButton">Higher</button>
         <button id="lowerButton">Lower</button>
       </div>
@@ -27,26 +40,40 @@ function Game({onEnd, firstSong, secondSong}) {
 };
 
 function App() {
+  const sampleTrack: Track = {
+    id: 1,
+    name: 'Song Title'
+  };
+
   const [showStartButton, setShowStartButton] = React.useState(true);
   const [showGame, setShowGame] = React.useState(false);
-  const [firstSong, setFirstSong] = React.useState('');
-  const [secondSong, setSecondSong] = React.useState('');
+  const [firstSong, setFirstSong] = useState<Track>(sampleTrack);
+  const [secondSong, setSecondSong] = useState<Track>(sampleTrack);
 
-  const handleStart = async () => {
+
+  const handleStart: () => void = async () => {
     setShowGame(true);
     setShowStartButton(false);
 
     try {
       console.log("handling starting the game");
       const { data } = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/startGame`);
-      setFirstSong(data[0].track.name);
-      setSecondSong(data[1].track.name);
+      const song1: Track = {
+        name: data[0].track.name,
+        id: data[0].track.id
+      };
+      const song2: Track = {
+        name: data[1].track.name,
+        id: data[1].track.id
+      }
+      setFirstSong(song1);
+      setSecondSong(song2);
     } catch (error) {
       console.error('Error getting first two songs:', error);
     }
   };
 
-  const handleEnd = () => {
+  const handleEnd: () => void = () => {
     setShowGame(false);
     setShowStartButton(true);
   }
